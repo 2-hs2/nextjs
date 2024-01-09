be6f7fc5638b9c347f95e3d033ccd69c055ae983
hyeseung <wjoo3@naver.com>
2024-01-09 10:24:58 +0900

    포스트 데이터를 가져와서 보여주기

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -29,4 +31,37 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  // pre-rendering.md 파일 혹은 ssg-ssr.md 파일 경로 (인자에 따라)
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML String
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,50 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from "react";
 
-const Post = () => {
-  return <div></div>;
+import homeStyles from "../../styles/Home.module.css";
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds(); // posts.ts에 존재하는 함수
+  // [ { params: {  id: 'pre-rendering' } }, { params: { id: 'ssg-ssr' } } ]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string); // type assertion으로 params.id가 string임을 명시
+  return {
+    props: {
+      postData,
+    },
+  };
+};
